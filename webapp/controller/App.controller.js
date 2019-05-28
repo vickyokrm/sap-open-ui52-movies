9b5c27d7dfc6341e3ef4e16125f56c9032c30453
Vigneshwaran Odayappan <vickyokrm@gmail.com>
2019-05-28 20:14:09 +0200

    Application setup

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("openui5.movies.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
