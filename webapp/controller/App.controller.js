c8c23ce025e5957394e59cc7351b2413fee7c3cd
Vigneshwaran Odayappan <vickyokrm@gmail.com>
2019-05-29 14:21:40 +0200

    Extended the application till w2u3

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,53 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/base/Log",
+	"../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+
+], function (Controller, Log, formatter, Filter, FilterOperator) {
 	"use strict";
-
 	return Controller.extend("openui5.movies.controller.App", {
-		onInit: function () {
+		formatter: formatter,
 
+		onInit: function () {
+			//Log.info('controller has been instantiated..')
+		},
+		onBeforeRendering: function () {
+			//Log.info("The view will shortly be rendered.");
+		},
+
+		onAfterRendering: function () {
+			//Log.info("The view has been rendered.");
+		},
+		onExit: function () {
+			//Log.info('controller will be destroyed..')
+		},
+		onPress: function (sValue) {
+			sap.ui.require(["sap/m/MessageToast"], function (oMessage) {
+				var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+				oMessage.show(oResourceBundle.getText("search") + sValue);
+			}.bind(this));
+			var sCity = this.byId('city').getValue(),
+				sGenre = this.byId('genre').getSelectedItem().getKey(),
+				oCalendar = this.byId("calendar"),
+				oRowBinding = oCalendar.getBinding("rows"),
+				oFilterGenre,
+				oFilterCity;
+
+			// Create filters for genre and city according to user inputs
+			oFilterGenre = sGenre ? new Filter("genre", FilterOperator.EQ, sGenre) : null;
+			oFilterCity = sCity ? new Filter("info", FilterOperator.Contains, sCity) : null;
+
+			// Apply genre filter to calendar rows
+			oRowBinding.filter(oFilterGenre);
+
+			// Apply city filter to row appointments
+			var aRows = oCalendar.getAggregation("rows");
+			aRows.forEach(function (oItem) {
+				var oAppointmentsBinding = oItem.getBinding("appointments");
+				oAppointmentsBinding.filter(oFilterCity);
+			});
 		}
 	});
 });
